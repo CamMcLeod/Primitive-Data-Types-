3224c0732cf87c4dba9c6ad85084d4fb68c59893
Cam McLeod <cammcleod3@gmail.com>
2019-05-15 11:31:34 -0700

    Completion of choose which operation to perform step

--- Calculator/main.m
+++ Calculator/main.m
@@ -8,12 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
+enum OperationType {add, subtract, multiply, divide};
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSInteger firstNumber = 7;
         NSInteger secondNumber = 2;
-        NSInteger result = firstNumber * secondNumber;
+        NSInteger result = 0;
+        
+        enum OperationType currentOperation = multiply;
+
+        switch (currentOperation) {
+            case add:
+                result = firstNumber + secondNumber;
+                break;
+            case subtract:
+                result = firstNumber - secondNumber;
+                break;
+            case multiply:
+                result = firstNumber * secondNumber;
+                break;
+            case divide:
+                result = firstNumber / secondNumber;
+                break;
+        }
+        
         NSLog(@"First number is: %li, Second number is: %li, Result is: %li", firstNumber, secondNumber, result);
     }
     return 0;
