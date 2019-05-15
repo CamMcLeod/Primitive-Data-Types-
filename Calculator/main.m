1fc853e3e3d29036b1e3933eb3f54c9317be52ec
Cam McLeod <cammcleod3@gmail.com>
2019-05-15 11:22:33 -0700

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Cameron Mcleod on 2019-05-15.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
